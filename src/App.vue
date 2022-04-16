f29bddaab66e3665028ab080ed2bba4a2bf42b0b
Yakov Kondratev <kondratev@nerve.ai>
2022-04-16 13:08:16 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,204 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header class="header">
+    <div class="container">
+      <div class="header_inner">
+        <div class="logo">
+          <a href="/"><img src="./assets/img/@2x/logo.png" alt="logo" /></a>
+        </div>
+        <a class="favorite_link" href="/favorite.html"
+          >Избранные котики
+          <svg
+            width="16"
+            height="14"
+            viewBox="0 0 16 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M7.59056 13.8381L1.44808 8.00922C-0.482693 6.17704 -0.482693 3.20132 1.44808 1.36913C3.24062 -0.331881 6.06651 -0.447887 8 1.02111C9.93349 -0.447887 12.7594 -0.331881 14.5519 1.36913C16.4827 3.20132 16.4827 6.17704 14.5519 8.00922L8.40944 13.8381C8.1819 14.054 7.8181 14.054 7.59056 13.8381ZM2.26705 2.18038C0.805427 3.56738 0.805427 5.81097 2.26705 7.19796L8.0001 12.6383L13.7332 7.19796C15.1948 5.81097 15.1948 3.56738 13.7332 2.18038C12.2645 0.786714 9.8782 0.786714 8.40954 2.18038C8.182 2.3963 7.8182 2.3963 7.59067 2.18038C6.122 0.786714 3.73572 0.786714 2.26705 2.18038Z"
+              fill="#626262"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </header>
+
+  <div class="sort_inner">
+    <div class="container">
+      <div class="sort_nav">
+        <div class="sort_brends">Породы</div>
+        <div class="sort_abc">
+          <div class="sort_abc__text">Сортировка по алфавиту</div>
+          <label class="switch">
+            <input type="checkbox" />
+            <span class="slider round"></span>
+          </label>
+        </div>
+      </div>
+      <div class="breads">
+        <div class="breads__all breads_btn">Все пёсели</div>
+        <div class="breads_inner">
+          <div class="breads_btn">Affenpinscher</div>
+          <div class="breads_btn">Briard</div>
+          <div class="breads_btn">Bulldog</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <main class="main">
+    <div class="container">
+      <div class="dogs_list">
+        <div class="dog_item">
+          <div class="dog_item__heard">
+            <img src="./assets/img/heard_big.svg" alt="alt" />
+          </div>
+          <div class="dog_item__img">
+            <img src="./assets/img/@2x/dog_01.jpg" alt="alt" />
+          </div>
+          <div class="dog_item__name">Maltese</div>
+        </div>
+        <div class="dog_item">
+          <div class="dog_item__heard">
+            <img src="./assets/img/heard_big.svg" alt="alt" />
+          </div>
+          <div class="dog_item__img">
+            <img src="./assets/img/@2x/dog_02.jpg" alt="alt" />
+          </div>
+          <div class="dog_item__name">Maltese</div>
+        </div>
+        <div class="dog_item">
+          <div class="dog_item__heard">
+            <img src="./assets/img/heard_big.svg" alt="alt" />
+          </div>
+          <div class="dog_item__img">
+            <img src="./assets/img/@2x/dog_03.jpg" alt="alt" />
+          </div>
+          <div class="dog_item__name">Maltese</div>
+        </div>
+        <div class="dog_item">
+          <div class="dog_item__heard">
+            <img src="./assets/img/heard_big.svg" alt="alt" />
+          </div>
+          <div class="dog_item__img">
+            <img src="./assets/img/@2x/dog_04.jpg" alt="alt" />
+          </div>
+          <div class="dog_item__name">Maltese</div>
+        </div>
+        <div class="dog_item">
+          <div class="dog_item__heard">
+            <img src="./assets/img/heard_big.svg" alt="alt" />
+          </div>
+          <div class="dog_item__img">
+            <img src="./assets/img/@2x/dog_01.jpg" alt="alt" />
+          </div>
+          <div class="dog_item__name">Maltese</div>
+        </div>
+        <div class="dog_item">
+          <div class="dog_item__heard">
+            <img src="./assets/img/heard_big.svg" alt="alt" />
+          </div>
+          <div class="dog_item__img">
+            <img src="./assets/img/@2x/dog_02.jpg" alt="alt" />
+          </div>
+          <div class="dog_item__name">Maltese</div>
+        </div>
+        <div class="dog_item">
+          <div class="dog_item__heard">
+            <img src="./assets/img/heard_big.svg" alt="alt" />
+          </div>
+          <div class="dog_item__img">
+            <img src="./assets/img/@2x/dog_03.jpg" alt="alt" />
+          </div>
+          <div class="dog_item__name">Maltese</div>
+        </div>
+        <div class="dog_item">
+          <div class="dog_item__heard">
+            <img src="./assets/img/heard_big.svg" alt="alt" />
+          </div>
+          <div class="dog_item__img">
+            <img src="./assets/img/@2x/dog_04.jpg" alt="alt" />
+          </div>
+          <div class="dog_item__name">Maltese</div>
+        </div>
+        <div class="dog_item">
+          <div class="dog_item__heard">
+            <img src="./assets/img/heard_big.svg" alt="alt" />
+          </div>
+          <div class="dog_item__img">
+            <img src="./assets/img/@2x/dog_01.jpg" alt="alt" />
+          </div>
+          <div class="dog_item__name">Maltese</div>
+        </div>
+        <div class="dog_item">
+          <div class="dog_item__heard">
+            <img src="./assets/img/heard_big.svg" alt="alt" />
+          </div>
+          <div class="dog_item__img">
+            <img src="./assets/img/@2x/dog_02.jpg" alt="alt" />
+          </div>
+          <div class="dog_item__name">Maltese</div>
+        </div>
+        <div class="dog_item">
+          <div class="dog_item__heard">
+            <img src="./assets/img/heard_big.svg" alt="alt" />
+          </div>
+          <div class="dog_item__img">
+            <img src="./assets/img/@2x/dog_03.jpg" alt="alt" />
+          </div>
+          <div class="dog_item__name">Maltese</div>
+        </div>
+        <div class="dog_item">
+          <div class="dog_item__heard">
+            <img src="./assets/img/heard_big.svg" alt="alt" />
+          </div>
+          <div class="dog_item__img">
+            <img src="./assets/img/@2x/dog_04.jpg" alt="alt" />
+          </div>
+          <div class="dog_item__name">Maltese</div>
+        </div>
+        <div class="dog_item">
+          <div class="dog_item__heard">
+            <img src="./assets/img/heard_big.svg" alt="alt" />
+          </div>
+          <div class="dog_item__img">
+            <img src="./assets/img/@2x/dog_01.jpg" alt="alt" />
+          </div>
+          <div class="dog_item__name">Maltese</div>
+        </div>
+      </div>
+      <div class="loader">
+        <div class="lds-ellipsis">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "assets/styles.min.css";
+
+body {
+  background: #f9f9f9;
+}
+.logo img {
+  width: 140px;
+}
+.lds-ellipsis div {
+  background: #3d24fb;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.favorite_link:hover {
+  color: #3d24fb;
+  svg path {
+    fill: #3d24fb;
   }
 }
 </style>
